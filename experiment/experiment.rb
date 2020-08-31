c7199fcdf5cd85e1d534a47191039e7f3cb2b2de
Matúš Sulír <matus.sulir@gmail.com>
2020-08-31 19:11:43 +0200

    Remove unnecessary code

--- experiment/experiment.rb
+++ experiment/experiment.rb
@@ -1,11 +1,7 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'bundler/setup'
-
 require 'agent'
 require 'csv'
 require 'fileutils'
-require 'webrick'
 require_relative 'service'
 
 class Experiment
@@ -88,13 +84,5 @@ class Experiment
 end
 
 if __FILE__ == $PROGRAM_NAME
-  if ARGV.include?('-service')
-    server = Thread.new { WEBrick::HTTPServer.new(DocumentRoot: Dir.pwd).start }
-    Experiment.new.run
-    sleep 1
-    `tar -czf build.tar.gz *`
-    server.join
-  else
-    Experiment.new.run
-  end
+  Experiment.new.run
 end
